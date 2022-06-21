ec35f9836d7c1db4be64bd4223a65b6f0b3ce3f7
João Pedro Gomes Ferreira <jppromus@gmail.com>
2022-06-21 20:01:43 -0300

    Deleted placeholder

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -85,7 +85,6 @@ const HomeHeader = ({ onSearch }) => {
             style={{ width: 20, height: 20, marginRight: SIZES.base }}
           />
           <TextInput
-            placeholder="Search NFTs"
             style={{ flex: 1 }}
             onChangeText={onSearch}
           />
